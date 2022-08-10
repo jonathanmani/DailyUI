014a61260694c31a88a73817ffa98a7087770b8f
Jonathan Mani <jonathanmani@protonmail.com>
2022-08-10 11:25:46 -0400

    finished the layout

--- Day004:Calculator/tailwind.config.js
+++ Day004:Calculator/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,js}",
     "./dist/index.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightGrey': '#D4D4D2',
+        'darkGrey': '#1C1C1C',
+        'mediumGrey': '#505050',
+        'ios-orange': '#FF9500',
+      },
+    },
   },
   plugins: [],
 }
